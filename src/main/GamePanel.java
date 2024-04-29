bfa9cfee1a7098fa10702bbe8f963f2fd6301791
Domisalej6921 <dominickgeorge21@gmail.com>
2024-04-29 15:51:22 +0100

    Added game loop and added a 48x48 pixel square on screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,10 +2,12 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
@@ -25,5 +27,41 @@ public class GamePanel extends JPanel{
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        
+        while (gameThread != null) {
+            //1 UPDATE: update info
+            update();
+
+            //2 DRAW: render a new screen
+            repaint();
+        }
+
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
